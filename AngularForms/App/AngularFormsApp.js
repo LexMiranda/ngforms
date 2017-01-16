05db7d96f4c21e4a7f62810d3a7068e6fb4548f8
Lex Miranda <lexlopes9@hotmail.com>
2017-01-16 14:21:04 -0200

    first commit

---
+++ AngularForms/App/AngularFormsApp.js
@@ -0,0 +1,20 @@
+﻿var angularFormsApp = angular.module('angularFormsApp', ['ngRoute']);
+
+angularFormsApp.config(function($routeProvider) {
+    $routeProvider.when("/home", {
+        templateUrl: "App/Home.html",
+        controller:"HomeController"
+    })
+    .when("/newEmployeeForm", {
+        templateUrl:"App/employeeForm/efTemplate."
+    });
+
+
+});
+
+angularFormsApp.controller("HomeController", function($scope, $location) {
+    $scope.addNewEmployee = function() {
+        $location.path('/newEmployeeForm');
+    };
+
+})
---
+++ AngularForms/App/EmployeeForm/efService.js
@@ -0,0 +1,14 @@
+﻿angularFormsApp.factory('efService',
+    function() {
+        return {
+            employee: {
+                fullName: "Lex Miranda",
+                notes: "Preparação para entrevista",
+                departament: "TI",
+                perkCar: true,
+                perkStock: false,
+                perkSixWeeks: true,
+                payrollType: "none"
+            }
+        }
+    });
